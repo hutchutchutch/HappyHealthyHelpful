448089eb644f625f9e9887abc44946dd751b39e6
Hutch <hutchenbach@gmail.com>
2025-01-03 21:59:37 -0600

    Updated CTA

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vxU6XYn62n99YKmKz/htusEohSaILAG8NDqt2T+wdyE=\"\n}"
